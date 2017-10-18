92a97613599354c5eb04451caec16b9742c3dbe0
IwantByJavaDev <misio_b@wp.pl>
2017-10-18 22:10:38 +0200

    Using spring injection by "Injection method".
    1. Initialization class in mydocumentsDao-context.xml
    2. Using property and ref. in xml file.
    3. Inject implementation DocumentDao interface into
    InjectSearchEngineImpl

---
+++ document-repository/src/main/java/pl/com/apress/docrepo/service/document/DocumentDao.java
@@ -0,0 +1,7 @@
+package pl.com.apress.docrepo.service.document;
+
+import pl.com.apress.docrepo.model.Document;
+
+public interface DocumentDao {
+	public Document[] getAllDocuments();
+}
---
+++ document-repository/src/test/java/pl/com/apress/docrepo/service/MyDocumentsDaoTest.java
@@ -0,0 +1,46 @@
+package pl.com.apress.docrepo.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import pl.com.apress.docrepo.model.Document;
+import pl.com.apress.docrepo.model.Type;
+
+public class MyDocumentsDaoTest {
+	private ClassPathXmlApplicationContext context;
+	private SearchEngine engine;
+	private Type documentType;
+
+	@Before
+	public void setup() {
+		context = new ClassPathXmlApplicationContext("mydocumentsDao-context.xml");
+		engine = context.getBean(SearchEngine.class);
+		documentType = context.getBean("webType", Type.class);
+	}
+
+	@Test
+	public void testFindByType() {
+
+		List<Document> documents = engine.findByType(documentType);
+
+		assertNotNull(documents);
+		assertTrue(documents.size() == 1);
+		assertEquals(documentType.getName(), documents.get(0).getType().getName());
+		assertEquals(documentType.getDesc(), documents.get(0).getType().getDesc());
+		assertEquals(documentType.getExtension(), documents.get(0).getType().getExtension());
+	}
+
+	@Test
+	public void testListAll() {
+		List<Document> documents = engine.listAll();
+		assertNotNull(documents);
+		assertTrue(documents.size() == 4);
+	}
+}
